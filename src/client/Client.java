7a72289cb6e643b31181ec93beb47acbccf930e9
311nguyenbaohuy <311nguyenbaohuy@gmail.com>
2019-10-24 09:40:49 +0700

    get ip wan

--- src/client/Client.java
+++ src/client/Client.java
@@ -13,7 +13,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -29,21 +31,6 @@ public class Client {
     private TagReader reader;
     private TagWriter writer;
 
-    public String getPublicIP() throws IOException{
-
-        // Find public IP address 
-        String systemipaddress = ""; 
-
-        URL url_name = new URL("http://bot.whatismyipaddress.com"); 
-
-        BufferedReader sc = 
-        new BufferedReader(new InputStreamReader(url_name.openStream())); 
-
-        // reads system IPAddress 
-        systemipaddress = sc.readLine().trim(); 
-        return systemipaddress;
-    }
-    
     private List<User> getUsers(byte [] content){
         String string = new String(content);
         string = string.replace("<", "");
@@ -60,16 +47,16 @@ public class Client {
     }
     
     public Client() throws UnknownHostException, IOException {
-        new Listener(1).start();
+
         conn = new Socket(InetAddress.getLocalHost(), 9000);
         reader = new TagReader(conn.getInputStream());  
         writer = new TagWriter(conn.getOutputStream());   
 
-//        String[] request = {Tags.LOGIN, "<bao a 1.1.1.1>"};
+        String[] request = {Tags.LOGIN, "<bao 1 1.1.1.1>"};
 //        String[] request = {Tags.REGISTER, "<huyy 1>"};
 //        String[] request = {Tags.LOGOUT, "<huy>"};
 //        String[] request = {Tags.SEARCH, "<bao>"};
-            String[] request = {Tags.FIND_FRIEND, "<bao>"};
+//            String[] request = {Tags.FIND_FRIEND, "<bao>"};
 //            String[] request = {Tags.LOGOUT, "<bao>"};
 //        String[] request = {Tags.REQUEST, "<bao huyy>"};
         try {
@@ -78,6 +65,7 @@ public class Client {
             writer.flush();
             tv = reader.getTagValue();
             System.out.println(tv.getTag());
+            System.out.println(new String(tv.getContent()));
             List<User> users = getUsers(tv.getContent());
             for (User usr : users){
                 System.out.println(usr.getUser_name());
@@ -88,9 +76,11 @@ public class Client {
         }
     }
     
-    public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-        new Client();
+    public static void main(String[] args) throws UnknownHostException, MalformedURLException, IOException{
+
+           ParseRoute pr = ParseRoute.getInstance();
+            System.out.println( "Gateway: " + pr.getGateway() );
+            System.out.println( "IP: " + pr.getLocalIPAddress() );
+ 
     }
-    
 }
